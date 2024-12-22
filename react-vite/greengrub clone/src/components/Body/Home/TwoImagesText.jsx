997009a2f2cee595606d4498b02b84ebce6fb46c
karimhassan1 <karimhassan263372@gmail.com>
2024-12-22 13:14:43 +0500

    imageresponsive check

--- react-vite/greengrub clone/src/components/Body/Home/TwoImagesText.jsx
+++ react-vite/greengrub clone/src/components/Body/Home/TwoImagesText.jsx
@@ -2,12 +2,12 @@ const TwoImagesTextCheck = (dataChild) => {
   const { data } = dataChild;
   // console.log(data1);
   return (
-    <div className="lg:w-2/5  h-60 me-10 relative mb-5">
+    <div className="lg:w-2/5 lg:h-60 lg:me-10 relative m-2">
       <img src={data.bgImgPath} alt="" className="w-full h-full rounded-xl" />
       <div className="absolute top-0 w-1/2 h-full flex flex-col text-white justify-center items-center">
-        <h1 className="text-2xl">{data.heading}</h1>
-        <p className="text-3xl font-bold ms-5">{data.description}</p>
-        <button className="border p-2 rounded-xl mt-5 bg-white text-black">
+        <h1 className="text-xs lg:text-2xl">{data.heading}</h1>
+        <p className="lg:text-3xl lg:font-bold lg:ms-5">{data.description}</p>
+        <button className="text-sm border p-1 lg:p-2 rounded-xl lg:mt-5 bg-white text-black">
           {data.button}
         </button>
       </div>
@@ -17,7 +17,7 @@ const TwoImagesTextCheck = (dataChild) => {
 const TwoImagesText = (dataParent) => {
   const { data } = dataParent;
   return (
-    <div className="lg:h-72 flex flex-col lg:flex-row mt-12 mb-12 justify-center items-center">
+    <div className="lg:h-72 flex flex-col sm:flex-row mt-12 mb-12 justify-center items-center">
       {data.map((list, index) => {
         return <TwoImagesTextCheck data={list} key={index} />;
       })}
