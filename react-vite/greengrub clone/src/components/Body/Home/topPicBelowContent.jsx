3470eabe95bd6d8f2e397701d93885d46ee0fe80
karimhassan1 <karimhassan263372@gmail.com>
2024-12-22 13:07:14 +0500

    responsness check

--- react-vite/greengrub clone/src/components/Body/Home/topPicBelowContent.jsx
+++ react-vite/greengrub clone/src/components/Body/Home/topPicBelowContent.jsx
@@ -2,11 +2,13 @@ const TopPicBelowContent = (data) => {
   return (
     <>
       <div className="w-full lg:w-1/4 flex lg:flex-col justify-center items-center relative mt-8 lg:mt-0">
-        <div className="h-32 border-8 rounded-100-percent w-32 lg:absolute top-0 flex justify-center items-center bg-white">
+        <div className="h-32 border-8 rounded-100-percent me-2 w-32 lg:absolute lg:top-0 flex justify-center items-center bg-white">
           <img className="lg:w-3/4 lg:h-3/4" src={data.imgPath} alt="" />
         </div>
-        <div className="bg-search lg:h-3/4 w-full flex justify-center items-center flex-col rounded-xl">
-          <p className="mt-5 mb-5 font-bold text-xl">{data.mainHeading}</p>
+        <div className="w-3/4 bg-search lg:h-3/4  flex justify-center items-center flex-col rounded-xl">
+          <p className="lg:mt-5 lg:mb-5 font-bold text-sm lg:text-xl">
+            {data.mainHeading}
+          </p>
           <p className="text-sm ps-4 pe-4">{data.description}</p>
         </div>
       </div>
@@ -18,7 +20,7 @@ const TopPicBelowContent = (data) => {
 const TopPicBelowContentMain = (mainData) => {
   const { data } = mainData;
   return (
-    <div className="lg:h-96 flex flex-col lg:flex-row justify-around mt-8 flex-wrap ps-20 pe-20 mb-20  lg:ps-0 lg:pe-0">
+    <div className="lg:h-96 flex flex-col lg:flex-row justify-around mt-8 flex-wrap ps-1 pe-1 mb-20  lg:ps-0 lg:pe-0">
       {data.map((datachild, index) => {
         return <TopPicBelowContent {...datachild} key={index} />;
       })}
