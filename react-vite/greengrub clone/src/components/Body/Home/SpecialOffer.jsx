ae33dbe0639694ea8230ec8935778c8029f0455a
karimhassan1 <karimhassan263372@gmail.com>
2024-12-22 16:07:47 +0500

    specialoffer

--- react-vite/greengrub clone/src/components/Body/Home/SpecialOffer.jsx
+++ react-vite/greengrub clone/src/components/Body/Home/SpecialOffer.jsx
@@ -10,25 +10,25 @@ const SpecialOffer = (data) => {
     min <= 0 ? setMin(60) : setMin(min - 1);
   }, 60000);
   return (
-    <div className="h-600-px border relative">
+    <div className="h-auto border relative">
       <img src={data.bgImgPath} alt="" className="w-full h-full" />
-      <div className="absolute top-0 flex justify-center items-center h-3/4 flex-col w-1/2">
+      <div className="absolute top-0 flex justify-center items-center w-full h-full bg-white bg-opacity-20 lg:bg-opacity-0 flex-col lg:w-1/2">
         <SmallDivHavingPicDescription {...data.smallDivHavingPicDes} />
         <div>
-          <b className="border-black me-5 bg-white p-4 text-xl rounded-xl">
+          <b className="border-black me-5 bg-white  text-xs p-1 lg:p-4 md:text-xl rounded-xl">
             {data.days} days
           </b>
-          <b className="border bg-white me-5 p-4 text-xl rounded-xl">
+          <b className="border bg-white me-5 p-1 lg:p-4 text-xs md:text-xl rounded-xl">
             {data.hours} Hr
           </b>
-          <b className="border bg-white me-5 p-4 text-xl rounded-xl">
+          <b className="border bg-white me-5 p-1 lg:p-4 text-xs md:text-xl rounded-xl">
             {min} Min
           </b>
-          <b className="border bg-primary text-white p-4 text-xl rounded-xl">
+          <b className="border bg-primary text-white p-1 lg:p-4 text-xs md:text-xl rounded-xl">
             {sec} Sec
           </b>
         </div>
-        <button className="mt-24 border bg-primary p-5 text-white rounded-xl">
+        <button className="mt-24 border bg-primary mb-5 p-1 lg:p-5 text-xs text-white rounded-xl">
           {data.button}
         </button>
       </div>
