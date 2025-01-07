11fa8ef5d28e6873bd57f07a114759ba82fca186
karimhassan1 <karimhassan263372@gmail.com>
2025-01-07 12:47:49 +0500

    complete mobile nav

--- react-vite/greengrub clone/src/components/Header/HeadBottom.jsx
+++ react-vite/greengrub clone/src/components/Header/HeadBottom.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import {
   HomeDropDownData,
@@ -14,7 +14,7 @@ const NavItem = ({ navItemText, data1, dropDownOrMega }) => {
   const [show, isShow] = useState(false);
   return (
     <li
-      className="border-b-2 p-3"
+      className="border-b-2 p-3 lg:border-b-0 "
       onMouseEnter={() => {
         isShow(true);
       }}
@@ -42,7 +42,16 @@ const HeadBottom = () => {
       id="mobile-nav"
       className="border-b-4 bg-white lg:h-16 font-bold text-1xl hidden lg:block absolute top-0 left-0 w-60  lg:w-full h-full lg:relative"
     >
-      <ul className="flex flex-col lg:flex-row lg:items-center justify-around lg:h-full lg:w-3/5 lg:m-auto">
+      <ul className="flex flex-col lg:flex-row lg:items-center justify-around lg:h-full lg:w-4/5 lg:m-auto">
+        <li className="ms-5 mt-2 lg:hidden">
+          <button
+            onClick={() => {
+              document.getElementById("mobile-nav").classList.add("hidden");
+            }}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} size="xl" />
+          </button>
+        </li>
         <NavItem
           navItemText={"Home"}
           data1={HomeDropDownData}
@@ -61,17 +70,17 @@ const HeadBottom = () => {
           dropDownOrMega={true}
         />
 
-        <li className="me-3 border-b-2 p-3">
+        <li className="me-3 border-b-2 p-3 lg:border-b-0">
           <Link to={"/Github"} className="lg:border-b-2 lg:ps-2 lg:be-2">
             My GitHub
           </Link>
         </li>
-        <li className="border-b-2 p-3">
+        <li className="border-b-2 p-3 lg:border-b-0">
           <Link to={"/occodion"} className="lg:border-b-2 lg:ps-2 lg:be-2">
             Occodion
           </Link>
         </li>
-        <li className="border-b-2 p-3">
+        <li className="border-b-2 p-3 lg:border-b-0">
           <Link to={"/context"} className="lg:border-b-2 lg:ps-2 lg:be-2">
             Context
           </Link>
